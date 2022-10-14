f2e23b8fa9a8e00c1bf4b42f3a8d9cac6a32032f
Sergey Filatyev <filatevsergey2407@gmail.com>
2022-10-14 23:53:09 +0300

    in-progress

--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
 import './css/styles.css';
-import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import NewApiService from './api-service';
+import refs from './refs';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const DEBOUNCE_DELAY = 300;
-axios('https://pixabay.com/api/?key=29648653-4a0943b69a497c54fdb933d14').then(
-  console.log
-);
+refs.search.addEventListener('click', onSearch);
+refs.loadMore.addEventListener('click', onLoadMore);
+
+const simpleLightbox = new SimpleLightbox('.gallery a', {});
+const newApiService = new NewApiService();
+
+async function onSearch() {
+  newApiService.searchQuery = refs.input.value;
+  newApiService.resetPage();
+  refs.gallery.innerHTML = '';
+  event.preventDefault();
+  if (!newApiService.searchQuery) {
+    refs.loadMore.classList.add('hidden');
+    return Notify.info(
+      'The input field must not be empty. Please enter something.'
+    );
+  }
+  const dataPictures = await newApiService.fetchPictures();
+  const pictures = dataPictures.data.hits;
+  const totalPictures = dataPictures.data.totalHits;
+  simpleLightbox.refresh();
+
+  if (pictures.length === 0) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  } else {
+    Notify.success(`Hooray! We found ${totalPictures} images.`);
+  }
+  if (refs.gallery.innerHTML) {
+    refs.loadMore.classList.remove('hidden');
+  }
+}
+
+async function onLoadMore() {
+  const dataPictures = await newApiService.fetchPictures();
+  newApiService.incrementPage();
+  simpleLightbox.refresh();
+}
---
+++ src/refs.js
@@ -0,0 +1,6 @@
+export default refs = {
+  search: document.querySelector('.search'),
+  input: document.querySelector('[name=searchQuery]'),
+  gallery: document.querySelector('.gallery'),
+  loadMore: document.querySelector('.load-more'),
+};
