959bbf898bf7ff24658b670c73ce04f8745868d5
Sergey Filatyev <filatevsergey2407@gmail.com>
2022-10-15 14:18:26 +0300

    sdf

--- src/refs.js
+++ src/refs.js
@@ -1,6 +1,7 @@
-export default refs = {
+const refs = {
   search: document.querySelector('.search'),
   input: document.querySelector('[name=searchQuery]'),
   gallery: document.querySelector('.gallery'),
   loadMore: document.querySelector('.load-more'),
 };
+export default refs;
